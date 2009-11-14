6121d51b4a70fdab0efc1a37c1faac4668da6f07
mh <mh@immerda.ch>
2009-11-14 15:20:54 +0100

    update to 0.25.x style

--- manifests/base.pp
+++ manifests/base.pp
@@ -4,8 +4,8 @@ class onak::base {
   }
 
   file{'/etc/onak.conf':
-      source => [ "puppet://$server/files/onak/${fqdn}/onak.conf",
-                  "puppet://$server/files/onak/onak.conf",
+      source => [ "puppet://$server/modules/site-onak/${fqdn}/onak.conf",
+                  "puppet://$server/modules/site-onak/onak.conf",
                   "puppet://$server/modules/onak/onak.conf" ],
       require => Package['onak'],
       notify => Service['onak'],
