5f67d41cd2bc7c396a1659a50ffb1bb3e555400b
mh <mh@immerda.ch>
2010-10-23 01:26:54 +0200

    quote url

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -4,8 +4,8 @@ define onak::nagios(
 ){
   nagios::service{
     "hkp_${name}":
-      check_command => "check_http_port_url_content!${name}!11371!/pks/lookup?op=get&search=${keyid}!${first_keyline}";
+      check_command => "check_http_port_url_content!${name}!11371!'/pks/lookup?op=get&search=${keyid}'!${first_keyline}";
     "hkps_${name}":
-      check_command => "check_https_port_url_content!${name}!11372!/pks/lookup?op=get&search=${keyid}!${first_keyline}";
+      check_command => "check_https_port_url_content!${name}!11372!'/pks/lookup?op=get&search=${keyid}'!${first_keyline}";
   }
 }
