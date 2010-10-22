f1aa0e9563a55c51b4c2f6d0fd54733e62648ce4
mh <mh@immerda.ch>
2010-10-23 00:50:30 +0200

    add nagios tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,4 +10,14 @@ class onak {
   if $use_shorewall {
     include shorewall::rules::keyserver
   }
+  if $use_nagios {
+    case $onak_keyserver_host { '': { fail("you need to set \$onak_keyserver_host on $fqdn to get nagios tests working") } }
+    case $onak_nagios_first_keyline { '': { fail("you need to set \$onak_nagios_first_keyline on $fqdn to get nagios tests working") } }
+    case $onak_nagios_keyid { '': { fail("you need to set \$onak_nagios_keyid on $fqdn to get nagios tests working") } }
+
+    onak::nagios{$onak_keyserver_host:
+      keyid => $onak_nagios_keyid,
+      first_keyline => $onak_nagios_first_keyline,
+    }
+  }
 }
