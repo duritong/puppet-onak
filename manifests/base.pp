826c0e291faa22994f1b098a37ea41540d943b03
mh <mh@immerda.ch>
2012-08-26 18:06:19 +0200

    whitespace

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,22 +1,22 @@
 class onak::base {
   package{'onak':
-      ensure => installed,
+    ensure => installed,
   }
 
   file{'/etc/onak.conf':
-      source => [ "puppet:///modules/site_onak/${::fqdn}/onak.conf",
-                  "puppet:///modules/site_onak/onak.conf",
-                  "puppet:///modules/onak/onak.conf" ],
-      require => Package['onak'],
-      notify => Service['onak'],
-      owner => root, group => 0, mode => 0644;
+    source => [ "puppet:///modules/site_onak/${::fqdn}/onak.conf",
+                "puppet:///modules/site_onak/onak.conf",
+                "puppet:///modules/onak/onak.conf" ],
+    require => Package['onak'],
+    notify => Service['onak'],
+    owner => root, group => 0, mode => 0644;
   }
 
   service{onak:
-      ensure => running,
-      enable => true,
-      hasstatus => true,
-      require => Package[onak],
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    require => Package[onak],
   }
 
   include onak::logrotate
