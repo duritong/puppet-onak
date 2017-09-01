5c48624bc4be88f2bee47d9701f5a83f923543d2
mh <mh@immerda.ch>
2017-09-01 19:14:48 +0200

    max_check_attempts should be undef by default

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -1,35 +1,38 @@
+# a simple check for nagios
 define onak::nagios(
-  $ensure = 'present',
   $keyid,
   $first_keyline,
-  $protocol = 'both',
-  $max_check_attempts = ''
+  $ensure             = 'present',
+  $protocol           = 'both',
+  $max_check_attempts = undef
 ){
 
   if !keyid and !$first_keyline {
     fail("You need to pass \$first_keyline and \$keyid")
   }
 
+  $hkp_ensure = $ensure ? {
+    'present' => $protocol ? {
+      'hkps'  => 'absent',
+      default => 'present',
+    },
+    default => $ensure,
+  }
+  $hkps_ensure = $ensure ? {
+    'present' => $protocol ? {
+      'hkp'   => 'absent',
+      default => 'present',
+    },
+    default => $ensure,
+  }
   nagios::service{
     "hkp_${name}":
-      ensure => $ensure ? {
-        'present' => $protocol ? {
-          'hkps' => 'absent',
-          default => 'present',
-        },
-        default => $ensure,
-      },
+      ensure             => $hkp_ensure,
       max_check_attempts => $max_check_attempts,
-      check_command => "check_http_port_url_content!${name}!11371!'/pks/lookup?op=get&search=${keyid}'!${first_keyline}";
+      check_command      => "check_http_port_url_content!${name}!11371!'/pks/lookup?op=get&search=${keyid}'!${first_keyline}";
     "hkps_${name}":
-      ensure => $ensure ? {
-        'present' => $protocol ? {
-          'hkp' => 'absent',
-          default => 'present',
-        },
-        default => $ensure,
-      },
+      ensure             => $hkps_ensure,
       max_check_attempts => $max_check_attempts,
-      check_command => "check_https_port_url_content!${name}!11372!'/pks/lookup?op=get&search=${keyid}'!${first_keyline}";
+      check_command      => "check_https_port_url_content!${name}!11372!'/pks/lookup?op=get&search=${keyid}'!${first_keyline}";
   }
 }
